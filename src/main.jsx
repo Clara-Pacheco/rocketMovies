9e0a59d4d3a580361d03fdeb750fcf6f5d70b551
Clara <clarapachecodev@gmail.com>
2023-04-14 09:50:12 -0300

    Render MovieInfo component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-
+import { MovieInfo } from './Pages/MovieInfo/MovieInfo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MovieInfo />
   </React.StrictMode>,
 )
