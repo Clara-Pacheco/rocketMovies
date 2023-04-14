2911f69bd29b1ba97feadd91b2b2b6d870d9b87d
Clara <clarapachecodev@gmail.com>
2023-04-14 10:37:42 -0300

    Import ThemeProvider, GlobalStyles and theme and use it to buid the structure rendered in the screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global.js'
+import theme from './styles/theme.js'
+
+
 import { MovieInfo } from './Pages/MovieInfo/MovieInfo.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MovieInfo />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+        <MovieInfo />
+    </ThemeProvider>
   </React.StrictMode>,
 )
