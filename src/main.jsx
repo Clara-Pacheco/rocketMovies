1f2e5c0cbbedc8db729d65f9b0697ba55d89a458
Clara <clarapachecodev@gmail.com>
2023-04-15 20:28:29 -0300

    Import SignIn and SignUp component pages to alternate their renderization

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import theme from './styles/theme.js'
 
 
 import { MovieInfo } from './Pages/MovieInfo/MovieInfo.jsx'
+import { SignIn } from './Pages/SignIn/SignIn.jsx'
+import { SignUp } from './Pages/SignUp/SignUp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <MovieInfo />
+        <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
