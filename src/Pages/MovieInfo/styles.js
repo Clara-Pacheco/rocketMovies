a9735f939ddf71651ddcb25e400163cc15c1eba9
Clara <clarapachecodev@gmail.com>
2023-04-15 11:52:35 -0300

    Add stylization for MovieInfo css component

--- src/Pages/MovieInfo/styles.js
+++ src/Pages/MovieInfo/styles.js
@@ -4,4 +4,43 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
 
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 50px 123px;
+
+    > h1 {
+      font-weight: 400;
+      font-size: 32px;
+      line-height: 42px;
+
+    }
+
+    > button {
+      
+      width: 207px;
+      height: 48px;
+      border-radius: 8px;
+      border: none;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+
+      background-color: ${({ theme }) => theme.COLORS.PINK};
+      color: ${({ theme }) => theme.COLORS.MOVIEINFO_BUTTON_TEXT};
+
+      svg {
+        width: 16px;
+        height: 16px;
+        margin-right: 8px;
+      }
+    }
+
+  }
+
 `;
