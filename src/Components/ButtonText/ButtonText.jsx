3d68aa2642ea9e5ae51ee4c47ba58f5876c35038
Clara <clarapachecodev@gmail.com>
2023-04-15 20:09:52 -0300

    Create ButtonText component + components css for stylization

---
+++ src/Components/ButtonText/ButtonText.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./styles.js";
+
+export function ButtonText({icon:Icon,value, ...rest }){
+  return(
+    <Container
+      type="button"
+      {...rest}
+    >
+      {Icon && <Icon size={20} />}
+      {value}
+    </Container>
+  )
+}
---
+++ src/Components/ButtonText/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+
+  display: flex;
+  gap:5px;
+  background: none;
+  border: none;
+  
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 21px;
+  text-align: center;
+
+  margin-top: 42px;
+
+  color: ${({ theme }) => theme.COLORS.PINK};
+
+`;
