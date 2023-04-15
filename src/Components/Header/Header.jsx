02ef977276ee1d1b27e21e57a3c35bd708844339
Clara <clarapachecodev@gmail.com>
2023-04-15 10:24:49 -0300

    Import Container, Input and Profile css componets and build Header component passing the required props

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,24 @@
-import { Container } from './styles'
+import { Container, Input, Profile } from './styles'
 
 export function Header(){
   return (
     <Container >
+      <h1>RocketMovies</h1>
 
+      <Input
+        type="text"
+        placeholder='Pesquisar por título'
+        required
+      />
+     
+      <Profile>
+        <div>
+          <strong>Clara Pacheco</strong>
+          <span>Sair</span>
+        </div>
+
+        <img src="https://github.com/Clara-Pacheco.png" />
+      </Profile>
       
     </Container>
   )
