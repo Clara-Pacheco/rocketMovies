00b3d04d8188edfd7dfa2c2569919e8a9b0d46d7
Clara <clarapachecodev@gmail.com>
2023-04-15 11:52:00 -0300

    Import FiPlus icon from react-icons and keep building MovieInfo component page with h1, button and FiPlus component

--- src/Pages/MovieInfo/MovieInfo.jsx
+++ src/Pages/MovieInfo/MovieInfo.jsx
@@ -1,11 +1,21 @@
 import { Container } from './styles.js'
 
+import { FiPlus } from 'react-icons/fi'
+
 import { Header } from '../../Components/Header/Header.jsx'
 
 export function MovieInfo() {
   return (
     <Container>
       <Header />
+      <div>
+        <h1>Meus filmes</h1>
+        
+        <button>
+          <FiPlus />
+          Adicionar filme
+        </button>
+      </div>
     </Container>
   )
 }
