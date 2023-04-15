a5aaae79b5a85c133dc70fadd51e83f0eaa8b31b
Clara <clarapachecodev@gmail.com>
2023-04-15 17:02:20 -0300

    Add Content css component to MovieInfo page component

--- src/Pages/MovieInfo/MovieInfo.jsx
+++ src/Pages/MovieInfo/MovieInfo.jsx
@@ -1,9 +1,10 @@
-import { Container, Button } from './styles.js'
+import { Container, Button, Content } from './styles.js'
 
 import { FiPlus } from 'react-icons/fi'
 
 import { Header } from '../../Components/Header/Header.jsx'
 import { FilmNote } from '../../Components/FilmNote/FilmNote.jsx'
+import { Tag } from '../../Components/Tag/Tag.jsx'
 
 export function MovieInfo() {
   return (
@@ -17,45 +18,47 @@ export function MovieInfo() {
           Adicionar filme
         </Button>
       </div>
+      <Content>
+        <FilmNote 
+          title="Interestellar"
+          children="Pragas nas colheitas fizeram a civilização
+          humana regredir para uma sociedade agrária em futuro
+          de data desconhecida. Cooper, ex-piloto da NASA, tem
+          uma fazenda com sua família. Murphy, a filha de dez
+          anos de Cooper, acredita que seu quarto está
+          assombrado por um fantasma que tenta se comunicar
+          com ela. Pai e filha descobrem que o 'fantasma'
+          é uma inteligência desconhecida que está enviando
+          mensagens codificadas através de" 
+          value="Romance"
+        />
 
-      <FilmNote 
-        title="Interestellar"
-        children="Pragas nas colheitas fizeram a civilização
-        humana regredir para uma sociedade agrária em futuro
-        de data desconhecida. Cooper, ex-piloto da NASA, tem
-        uma fazenda com sua família. Murphy, a filha de dez
-        anos de Cooper, acredita que seu quarto está
-        assombrado por um fantasma que tenta se comunicar
-        com ela. Pai e filha descobrem que o 'fantasma'
-        é uma inteligência desconhecida que está enviando
-        mensagens codificadas através de" 
-      />
+        <FilmNote 
+          title="Interestellar"
+          children="Pragas nas colheitas fizeram a civilização
+          humana regredir para uma sociedade agrária em futuro
+          de data desconhecida. Cooper, ex-piloto da NASA, tem
+          uma fazenda com sua família. Murphy, a filha de dez
+          anos de Cooper, acredita que seu quarto está
+          assombrado por um fantasma que tenta se comunicar
+          com ela. Pai e filha descobrem que o 'fantasma'
+          é uma inteligência desconhecida que está enviando
+          mensagens codificadas através de" 
+        />
 
-      <FilmNote 
-        title="Interestellar"
-        children="Pragas nas colheitas fizeram a civilização
-        humana regredir para uma sociedade agrária em futuro
-        de data desconhecida. Cooper, ex-piloto da NASA, tem
-        uma fazenda com sua família. Murphy, a filha de dez
-        anos de Cooper, acredita que seu quarto está
-        assombrado por um fantasma que tenta se comunicar
-        com ela. Pai e filha descobrem que o 'fantasma'
-        é uma inteligência desconhecida que está enviando
-        mensagens codificadas através de" 
-      />
-
-      <FilmNote 
-        title="Interestellar"
-        children="Pragas nas colheitas fizeram a civilização
-        humana regredir para uma sociedade agrária em futuro
-        de data desconhecida. Cooper, ex-piloto da NASA, tem
-        uma fazenda com sua família. Murphy, a filha de dez
-        anos de Cooper, acredita que seu quarto está
-        assombrado por um fantasma que tenta se comunicar
-        com ela. Pai e filha descobrem que o 'fantasma'
-        é uma inteligência desconhecida que está enviando
-        mensagens codificadas através de" 
-      />
+        <FilmNote 
+          title="Interestellar"
+          children="Pragas nas colheitas fizeram a civilização
+          humana regredir para uma sociedade agrária em futuro
+          de data desconhecida. Cooper, ex-piloto da NASA, tem
+          uma fazenda com sua família. Murphy, a filha de dez
+          anos de Cooper, acredita que seu quarto está
+          assombrado por um fantasma que tenta se comunicar
+          com ela. Pai e filha descobrem que o 'fantasma'
+          é uma inteligência desconhecida que está enviando
+          mensagens codificadas através de" 
+        />
+      </Content>
     </Container>
   )
 }
