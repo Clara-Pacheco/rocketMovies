1a93aa31429b7d6ccc2262db7939f62842193a18
Clara <clarapachecodev@gmail.com>
2023-04-15 20:16:03 -0300

    Add stylization for Button component container

--- src/Components/Button/styles.js
+++ src/Components/Button/styles.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
+  width: 100%; 
+  height: 56px;
 
+  border: 0;
+  border-radius: 10px;
+  margin-top: 24px;
 
+  background-color: ${({ theme }) => theme.COLORS.PINK};
+  color: ${({ theme }) => theme.COLORS.BACKGROUND_TAG};
 
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 21px;
+  text-align: center;
 
 
 `;
