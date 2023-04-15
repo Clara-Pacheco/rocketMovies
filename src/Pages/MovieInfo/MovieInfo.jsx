b410cba15c216631d83cd364f744fca3f29543d3
Clara <clarapachecodev@gmail.com>
2023-04-15 16:13:23 -0300

    Import FilmNote component and use in the construction of MovieInfo page component

--- src/Pages/MovieInfo/MovieInfo.jsx
+++ src/Pages/MovieInfo/MovieInfo.jsx
@@ -3,6 +3,7 @@ import { Container, Button } from './styles.js'
 import { FiPlus } from 'react-icons/fi'
 
 import { Header } from '../../Components/Header/Header.jsx'
+import { FilmNote } from '../../Components/FilmNote/FilmNote.jsx'
 
 export function MovieInfo() {
   return (
@@ -16,6 +17,45 @@ export function MovieInfo() {
           Adicionar filme
         </Button>
       </div>
+
+      <FilmNote 
+        title="Interestellar"
+        children="Pragas nas colheitas fizeram a civilização
+        humana regredir para uma sociedade agrária em futuro
+        de data desconhecida. Cooper, ex-piloto da NASA, tem
+        uma fazenda com sua família. Murphy, a filha de dez
+        anos de Cooper, acredita que seu quarto está
+        assombrado por um fantasma que tenta se comunicar
+        com ela. Pai e filha descobrem que o 'fantasma'
+        é uma inteligência desconhecida que está enviando
+        mensagens codificadas através de" 
+      />
+
+      <FilmNote 
+        title="Interestellar"
+        children="Pragas nas colheitas fizeram a civilização
+        humana regredir para uma sociedade agrária em futuro
+        de data desconhecida. Cooper, ex-piloto da NASA, tem
+        uma fazenda com sua família. Murphy, a filha de dez
+        anos de Cooper, acredita que seu quarto está
+        assombrado por um fantasma que tenta se comunicar
+        com ela. Pai e filha descobrem que o 'fantasma'
+        é uma inteligência desconhecida que está enviando
+        mensagens codificadas através de" 
+      />
+
+      <FilmNote 
+        title="Interestellar"
+        children="Pragas nas colheitas fizeram a civilização
+        humana regredir para uma sociedade agrária em futuro
+        de data desconhecida. Cooper, ex-piloto da NASA, tem
+        uma fazenda com sua família. Murphy, a filha de dez
+        anos de Cooper, acredita que seu quarto está
+        assombrado por um fantasma que tenta se comunicar
+        com ela. Pai e filha descobrem que o 'fantasma'
+        é uma inteligência desconhecida que está enviando
+        mensagens codificadas através de" 
+      />
     </Container>
   )
 }
