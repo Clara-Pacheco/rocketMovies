c1c306b1a800cefedcf166c61efe7bcc69003b7a
Clara <clarapachecodev@gmail.com>
2023-04-15 10:23:25 -0300

    Import styled from styled-components and build Container as div cc component, Input as input css component and Profile as div css component

--- src/Components/Header/styles.js
+++ src/Components/Header/styles.js
@@ -1,8 +1,97 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  grid-area: header;
+  
   width: 100%;
+  height: 116px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  padding: 24px 123px;
+  gap: 64px;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_HEADER};
+
+  > h1 {
+    font-family: 'Roboto Slab';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 32px;
+
+    color: ${({ theme }) => theme.COLORS.PINK};
+  }
+  `;
+
+export const Input = styled.input`
+
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 19px 24px;
+    border: none;
+
+    width: 630px;
+    height: 56px;
+
+    background-color: ${({ theme }) => theme.COLORS.INPUT_FIELD};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    border-radius: 10px;
+
+    &::placeholder {
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 18px;
+
+      color: ${({ theme }) => theme.COLORS.PLACEHOLDER_ICONS_COLOR};
+    }
+  `;
+
+  export const Profile = styled.div`
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      
+
+      width: 198px;
+      height: 98px;
+      font-size: 14px;
+      line-height: 18px;
+
+      > div {
+        display: flex;
+        flex-direction: column;
+        margin-right: 9px;
+      }
+
+      > strong { 
+        font-weight: 700;   
+        color: ${({ theme }) => theme.COLORS.PARAGRAPH_MOVIEINFO};
+      }
+
+      > span {
+        font-weight: 400; 
+        color: ${({ theme }) => theme.COLORS.PLACEHOLDER_ICONS_COLOR};
+      }
+
+      > img {
+        width: 64px;
+        height: 64px;
+        border-radius: 50%;
+
+        border: 1px solid ${({ theme }) => theme.COLORS.IMG_BORDER};
+      }
+  `;
 
 
 
-`;
+
+
+
+
+
+
