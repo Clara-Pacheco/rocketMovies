916af94a7db441b3e3984d20295abb2e17699f3c
Clara <clarapachecodev@gmail.com>
2023-04-15 17:00:15 -0300

    Remove margin 0 132px and add margin left to h1 and margin right to button + create content css component

--- src/Pages/MovieInfo/styles.js
+++ src/Pages/MovieInfo/styles.js
@@ -8,12 +8,13 @@ export const Container = styled.div`
     display: flex;
     justify-content: space-between;
     padding: 50px 123px;
-    margin: 0 132px;
+    
 
     > h1 {
       font-weight: 400;
       font-size: 32px;
       line-height: 42px;
+      margin-left: 123px;
 
     }
   }
@@ -25,6 +26,7 @@ export const Button = styled.button`
     height: 48px;
     border-radius: 8px;
     border: none;
+    margin-right: 106px;
 
     display: flex;
     justify-content: center;
@@ -39,6 +41,22 @@ export const Button = styled.button`
         height: 16px;
         margin-right: 8px;
       }
+    
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 716px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px 16px 0px 0px;
+  gap: 24px;
+
+
+  overflow-y: auto;
+
     
 `;
 
