98f688edc461a6634b84390f1d65ec172af215fc
Clara <clarapachecodev@gmail.com>
2023-04-17 16:58:53 -0300

    Create folder Textarea containing Textarea component and styles.js with css styles components

---
+++ src/Components/Textarea/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.textarea`
+  width: 100%;
+  height: 247px;
+
+  border-radius: 10px;
+  border: none;
+  resize: none;
+
+  background-color: ${({ theme }) => theme.COLORS.INPUT_FIELD};
+  color: ${({ theme }) => theme.COLORS.PLACEHOLDER_ICONS_COLOR};
+
+  padding: 19px 16px;
+  margin-bottom: 40px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.COLORS.PLACEHOLDER_ICONS_COLOR};
+  }
+
+`;
