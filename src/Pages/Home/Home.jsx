bb8c654f69aaa4e9cd558cfc5652a4f504bfa14c
Clara <clarapachecodev@gmail.com>
2023-04-17 17:01:34 -0300

    Create folder Home containing Home component page and styles.js with css components

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,86 @@
+import { Container, Button, Content, TagGroup, FilmInfo } from './styles.js'
+
+import { FiPlus } from 'react-icons/fi'
+
+import { Header } from '../../Components/Header/Header.jsx'
+import { FilmNote } from '../../Components/FilmNote/FilmNote.jsx'
+import { Tag } from '../../Components/Tag/Tag.jsx'
+
+export function Home() {
+  return (
+    <Container>
+      <Header />
+      <div>
+        <h1>Meus filmes</h1>
+        
+        <Button>
+          <FiPlus />
+          Adicionar filme
+        </Button>
+      </div>
+      <Content>
+      <FilmInfo>
+        <FilmNote 
+          title="Interestellar"
+          children="Pragas nas colheitas fizeram a civilização
+          humana regredir para uma sociedade agrária em futuro
+          de data desconhecida. Cooper, ex-piloto da NASA, tem
+          uma fazenda com sua família. Murphy, a filha de dez
+          anos de Cooper, acredita que seu quarto está
+          assombrado por um fantasma que tenta se comunicar
+          com ela. Pai e filha descobrem que o 'fantasma'
+          é uma inteligência desconhecida que está enviando
+          mensagens codificadas através de"  
+        />
+        <TagGroup>
+          <Tag value="Ficção Científica" />
+          <Tag  value= "Drama" />
+          <Tag value= "Família" />
+        </TagGroup>
+      </FilmInfo>
+
+      <FilmInfo>
+        <FilmNote 
+          title="Interestellar"
+          children="Pragas nas colheitas fizeram a civilização
+          humana regredir para uma sociedade agrária em futuro
+          de data desconhecida. Cooper, ex-piloto da NASA, tem
+          uma fazenda com sua família. Murphy, a filha de dez
+          anos de Cooper, acredita que seu quarto está
+          assombrado por um fantasma que tenta se comunicar
+          com ela. Pai e filha descobrem que o 'fantasma'
+          é uma inteligência desconhecida que está enviando
+          mensagens codificadas através de" 
+        />
+        <TagGroup>
+          <Tag value="Ficção Científica" />
+          <Tag  value= "Drama" />
+          <Tag value= "Família" />
+        </TagGroup>
+      </FilmInfo>
+
+      <FilmInfo>
+        <FilmNote 
+          title="Interestellar"
+          children="Pragas nas colheitas fizeram a civilização
+          humana regredir para uma sociedade agrária em futuro
+          de data desconhecida. Cooper, ex-piloto da NASA, tem
+          uma fazenda com sua família. Murphy, a filha de dez
+          anos de Cooper, acredita que seu quarto está
+          assombrado por um fantasma que tenta se comunicar
+          com ela. Pai e filha descobrem que o 'fantasma'
+          é uma inteligência desconhecida que está enviando
+          mensagens codificadas através de" 
+        />
+        <TagGroup>
+          <Tag value="Ficção Científica" />
+          <Tag  value= "Drama" />
+          <Tag value= "Família" />
+        </TagGroup>
+      </FilmInfo>  
+      </Content>
+    </Container>
+  )
+}
+
+
---
+++ src/Pages/Home/styles.js
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  > div {
+    display: flex;
+    justify-content: space-between;
+    padding: 50px 123px;
+    
+
+    > h1 {
+      font-weight: 400;
+      font-size: 32px;
+      line-height: 42px;
+      margin-left: 123px;
+
+    }
+  }
+
+`;
+
+export const Button = styled.button`
+    width: 207px;
+    height: 48px;
+    border-radius: 8px;
+    border: none;
+    margin-right: 106px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+
+    background-color: ${({ theme }) => theme.COLORS.PINK};
+    color: ${({ theme }) => theme.COLORS.MOVIEINFO_BUTTON_TEXT};
+
+      svg {
+        width: 16px;
+        height: 16px;
+        margin-right: 8px;
+      }
+    
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 716px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px 16px 0px 0px;
+  gap: 24px;
+
+`;
+
+export const TagGroup = styled.div`
+  display: flex;
+  margin-top: 20px;
+
+`;
+
+export const FilmInfo = styled.div`
+  padding: 32px;
+  margin: 0 132px;
+  gap: 15px;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_MYMOVIESCARDS};
+
+  overflow-y: auto;
+`;
