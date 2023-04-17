195fdb0b92cc1d197aa4ab91352e18798b60137e
Clara <clarapachecodev@gmail.com>
2023-04-17 18:27:24 -0300

    Add icon property to Tag component

--- src/Components/Tag/Tag.jsx
+++ src/Components/Tag/Tag.jsx
@@ -1,8 +1,9 @@
 import { Container } from './styles.js'
 
-export function Tag({ value, ...rest}){
+export function Tag({icon:Icon, value, ...rest}){
   return(
     <Container {...rest}>
+      {Icon && <Icon size={20} />}
       {value}
     </Container>
   )
