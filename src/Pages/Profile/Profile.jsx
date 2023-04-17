51702cb0b4cb970889aa0f54836d2dc9f5e9df21
Clara <clarapachecodev@gmail.com>
2023-04-17 09:22:53 -0300

    Build Profile component page

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,9 +1,61 @@
-import { Container } from '.styles.js'
+import { Container, Form, Avatar } from './styles'
 
-export function Container(){
+import { FiUser, FiMail, FiLock, FiCamera, FiArrowLeft } from 'react-icons/fi'
+
+import { Button } from '../../Components/Button/Button'
+import { Input } from '../../Components/Input/Input'
+import { ButtonText } from '../../Components/ButtonText/ButtonText'
+
+
+export function Profile(){
   return (
     <Container>
-      
+      <header>
+        <ButtonText 
+          value="Voltar"
+          icon={ FiArrowLeft }
+        />
+        </header>
+        <Form>
+          <Avatar>
+            <img
+              src='https://github.com/Clara-Pacheco.png'
+              alt='Foto de usuário'
+            />
+
+            <label htmlFor='avatar'>
+              <FiCamera />
+
+              <input
+                id='avatar'
+                type='file'
+              />
+
+            </label>
+          </Avatar>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+        <Input
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+        />
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+        />
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Salvar" />
+      </Form>
     </Container>
   )
 }
