b3a96f86eb8c70d4f3f4e2b8698df6123f9f6817
Clara <clarapachecodev@gmail.com>
2023-04-19 09:56:04 -0300

    Add prop 'isNew' to Tag component

--- src/Components/Tag/Tag.jsx
+++ src/Components/Tag/Tag.jsx
@@ -1,10 +1,10 @@
 import { Container } from './styles.js'
 
-export function Tag({icon:Icon, value, ...rest}){
+export function Tag({icon:Icon, value,isNew, ...rest}){
   return(
-    <Container {...rest}>
-      {Icon && <Icon size={20} />}
+    <Container {...rest} isNew={isNew}>
       {value}
+      {Icon && <Icon size={20} /> }   
     </Container>
   )
 }
