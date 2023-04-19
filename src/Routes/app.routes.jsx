676baf83cebb0dbd12ac48ac58bfa0dd6d9f9180
Clara <clarapachecodev@gmail.com>
2023-04-19 11:31:14 -0300

    Import Routes and Route from react router dom, import CreateMovie, Home, MoviePreview and Profile page components and use them to build AppRoutes components

---
+++ src/Routes/app.routes.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+
+import { CreateMovie } from '../Pages/CreateMovie/CreateMovie'
+import { Home } from '../Pages/Home/Home'
+import { MoviePreview } from '../Pages/MoviePreview/MoviePreview'
+import { Profile } from '../Pages/Profile/Profile'
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/createmovie" element={<CreateMovie />} />
+      <Route path="/preview/:id" element={<MoviePreview />} />
+      <Route path="/profile" element= {<Profile />} />
+  </Routes>
+  )
+ 
+}
