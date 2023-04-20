01a584a709795534949e3cda33400b22db8d727a
Clara <clarapachecodev@gmail.com>
2023-04-20 10:00:08 -0300

    Restore vite config

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // TODO: comment out before pushing to production
-  define: {
-    "global": {},
-  },
-  resolve: {
-    alias: {
-      './runtimeConfig': './runtimeConfig.browser',
-    },
-  },
-  plugins: [
-    react()
-  ]
+  plugins: [react()],
 })
+
+
