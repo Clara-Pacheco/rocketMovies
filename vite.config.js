64fe7b15c7f6864bf6714c468f6c896e790d4f7c
Clara <clarapachecodev@gmail.com>
2023-04-20 09:36:51 -0300

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // TODO: comment out before pushing to production
+  define: {
+    "global": {},
+  },
   resolve: {
     alias: {
       './runtimeConfig': './runtimeConfig.browser',
